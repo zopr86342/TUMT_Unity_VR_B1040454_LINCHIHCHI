73bdd051112d23a929bf8e1479658ab166656e77
zopr86342 <55218489+zopr86342@users.noreply.github.com>
2020-01-08 23:41:06 +0800

    場景已配置

---
+++ Unity_VR_B1040454_LINCHIHCHI/Assets/程式/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static bool haveKey;
+    public void ThrowPickUp()  //撿起
+    {
+
+    }
+    public void ThrowDetach(GameObject obj)  //放開
+    {
+        haveKey = true;
+        Destroy(obj);
+    }
+    public void ThrowHeld()  //持續拿著
+    {
+
+    }
+	
+}
---
+++ Unity_VR_B1040454_LINCHIHCHI/Assets/程式/Portal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Portal : MonoBehaviour {
+    public GameObject fnial;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other .tag == "鑰匙" && GameManager.haveKey)
+        {
+            print("已觸發");
+            GameObject.Find("結束畫面").SetActive(true);
+        }
+    }
+}
