1e7361a1525249611981581346c16f43a00cf8ad
zopr86342 <55218489+zopr86342@users.noreply.github.com>
2020-01-09 11:26:40 +0800

    最終

--- Unity_VR_B1040454_LINCHIHCHI/Assets/程式/GameManager.cs
+++ Unity_VR_B1040454_LINCHIHCHI/Assets/程式/GameManager.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-    public static bool haveKey;
+  
+
+
     public void ThrowPickUp()  //撿起
     {
 
     }
     public void ThrowDetach(GameObject obj)  //放開
     {
-        haveKey = true;
         Destroy(obj);
+
+
     }
     public void ThrowHeld()  //持續拿著
     {
